cf8bed762d3aee705e012b03b7b05df44d0b6ce9
Asus <Asus@DESKTOP-IFV2Q8B>
2018-10-13 15:55:33 +0700

    Start

---
+++ CorruptionManagement.Web/Controllers/ComplaintController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CorruptionManagement.Web.Controllers
+{
+    public class ComplaintController : Controller
+    {
+        // GET: Complaint
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ticket()
+        {
+            return View();
+        }
+    }
+}
